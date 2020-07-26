96c721cd6d19d9cc4b0512dfa0bfcd82c10c39e6
Megan Horsfield <meganthadev@gmail.com>
2020-07-26 05:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function fetchBooks() {
+function fetchBooks( {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => console.log(json));
 
-}
+})
 
 function renderBooks(books) {
   const main = document.querySelector('main')
